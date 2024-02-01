a0f77d8a37812b8cef7351c3fc71e364b4891040
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2024-02-01 09:36:57 +0800

    ok

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home",
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Asai",
         "email" => "ahmadsyaifuddinbusinessman.gmail.com"
     ]);
 });
 
-Route::get('/blog', function () {
-    return view('posts');
+Route::get('/posts', function () {
+    return view('posts', [
+        "title" => "Posts"
+    ]);
 });
