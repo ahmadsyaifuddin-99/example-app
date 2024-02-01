43c9363df6a55c55c6d4d632e78de068d2ed51cf
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2024-02-01 11:37:55 +0800

    ok

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.main')
 @section('container')
-    <h1>Halaman Posts</h1>
+    {{-- <h2>Judul</h2>
+    <h5>Asai</h5>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis ducimus neque, saepe praesentium id sint nam non
+        modi! Laudantium assumenda quibusdam ex culpa facilis sunt animi. Iure esse aperiam cum!</p> --}}
+
+    @foreach ($posts as $post)
+        <article class="mb-5">
+            <h2>
+                <a href="/posts/{{ $post['slug'] }}"> {{ $post['title'] }} </a>
+            </h2>
+            <h5>By: {{ $post['author'] }} </h5>
+            <p> {{ $post['body'] }} </p>
+        </article>
+    @endforeach
 @endsection
